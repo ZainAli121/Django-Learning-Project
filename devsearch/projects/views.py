0ac096099fbec64b4b3ce3b209ac6667bde0c4d6
Zain <learnerstudent001@gmail.com>
2023-09-25 22:40:07 +0500

    feat: Templates Data rendering

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -2,8 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def projects(request):
-    return render( request, 'projects/projects.html')
+projectList = [
+    {
+        'id': '1',
+        'title' : 'Portfolio Web',
+        'description': 'My personal portfolio website',
+        'topRated' : True
+    },
+    {
+        'id': '2',
+        'title' : 'Ecommerce Web',
+        'description': 'An ecommerce web build for the easy shopping',
+        'topRated' : False
+    },
+    {
+        'id': '3',
+        'title' : 'Blogging App',
+        'description': 'Blog app for posting blogs',
+        'topRated' : True
+    },
+]
 
-def project(request):
-    return render(request, 'projects/single-project.html')
+def projects(request):
+    context = {'projects' : projectList}
+    return render( request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObject = None
+    for i in projectList:
+        if i['id'] == str(pk):
+            projectObject = i
+    return render(request, 'projects/single-project.html', {'project' : projectObject})
