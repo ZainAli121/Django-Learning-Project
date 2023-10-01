41817a65a12485360e7e3bd6aae2ae125b92fe8c
Zain <learnerstudent001@gmail.com>
2023-10-01 10:37:33 +0500

    Database Relationships

--- devsearch/projects/models.py
+++ devsearch/projects/models.py
@@ -2,6 +2,8 @@ from django.db import models
 import uuid
 
 # Create your models here.
+
+# Project model 
 class Project(models.Model):
     # Owner field
     title = models.CharField(max_length=100)
@@ -11,8 +13,36 @@ class Project(models.Model):
     source_code = models.CharField(max_length=1000, null=True, blank=True)
     vote_total = models.IntegerField(default=0)
     vote_ratio = models.IntegerField(default=0)
+    tags = models.ManyToManyField('Tag', null=True, blank=True)  # Stringfy the model if it is defined below
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return self.title
+
+# A review model for the project 
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'up'),
+        ('down', 'down'),
+    )
+    # owner
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=50, choices=VOTE_TYPE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.value
+
+
+# Tag model for the project like django, react, python, java etc
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
