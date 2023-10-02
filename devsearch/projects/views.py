9280f0f3e0a218b2b6c8ae1c0b55e32da193d423
Zain <learnerstudent001@gmail.com>
2023-10-02 11:00:03 +0500

    feat: CRUD

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 # Create your views here.
 
 def projects(request):
@@ -14,3 +15,32 @@ def project(request, pk):
     reviews = projectObj.review_set.all()
     context = {'project' : projectObj, 'tags' : tags, 'reviews': reviews}
     return render(request, 'projects/single-project.html', context)
+
+def createProject(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect ('/')
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'projects/project-form.html', context)
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid:
+            form.save()
+            return redirect('/')
+    context = {'form': form}
+    return render(request, 'projects/project-form.html', context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    return render(request, 'projects/delete.html', {'object': project})
